a2e1096a702f690b76f894a50c6242f8183a0298
matheusned15 <matheusned15@gmail.com>
2020-11-05 17:39:35 -0300

    Criação da entidade Héroi

---
+++ src/main/java/br/com/jogo/api/heroi/Heroi.java
@@ -0,0 +1,51 @@
+package br.com.jogo.api.heroi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "HEROI")
+public class Heroi {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CODIGO")
+    private Long codigo;
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    @Column(name = "NOME")
+    private String nome;
+
+    @Column(name = "OUTRO_NOME")
+    private String outro_nome;
+
+    @NotNull
+    @Min(1)
+    @Column(name = "LEVEL")
+    private Integer level;
+
+    @Column(name = "IMAGEM")
+    private String imagem;
+
+    @Column(name = "PODERES")
+    private String poderes;
+
+}
